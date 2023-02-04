381acc102b88fd2334adaaa5f44cddc6eb253f1a
Domenico <Domenico@DESKTOP-251NCD4.lan>
2023-02-04 17:58:07 +0100

    *

--- src/main/java/com/inf/demo/controller/InfController.java
+++ src/main/java/com/inf/demo/controller/InfController.java
@@ -3,14 +3,11 @@ package com.inf.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigurationImportEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.inf.demo.dao.AnimeRepository;
--- src/main/webapp/WEB-INF/view/components/update-anime-form.jsp
+++ src/main/webapp/WEB-INF/view/components/update-anime-form.jsp
@@ -5,7 +5,7 @@
 <html>
 	<head>
 		<meta charset="ISO-8859-1">
-		<title>Insert title here</title>
+		<title>INF - Aggiorna anime</title>
 	</head>
 	<body>
 		<form action="/update-anime/${anime.getId()}" method="post">
